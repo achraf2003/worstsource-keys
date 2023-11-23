642b202d917a9d13fafdea669e345daed03356e0
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-11-23 13:46:03 -0300

    fix: Some changes to make sure

--- index.mjs
+++ index.mjs
@@ -3,55 +3,64 @@ import { Deobfuscator } from "deobfuscator"
 import { writeFile } from "node:fs/promises"
 import { assert } from "node:console"
 
-async function deobfuscate(obfuscatedScript) {
-// Phase 1: Webcrack
+async function deobfuscationLoop(obfuscatedInput, loopFunction) {
+    let deobfuscated = obfuscatedInput
     for (let run = 0; run < 5; run++) {
         try {
-            const result = await webcrack(obfuscatedScript)
-            if (result.code == "") break
-            obfuscatedScript = result.code
+            const result = await loopFunction(deobfuscated)
+            if (result == "" || result == undefined) break
+            deobfuscated = result
         } catch (e) {
+            console.error(e)
             break
         }
     }
+    return deobfuscated
+}
 
-    // Phase 2: Synchrony
-    const synchrony = new Deobfuscator()
-    let synchronyDeobfs = obfuscatedScript
-    for (let run = 0; run < 5; run++) {
-        try {
-            const result = await synchrony.deobfuscateSource(synchronyDeobfs)
-            if (result == "") break
-            synchronyDeobfs = result
-        } catch (e) {
-            break
+async function deobfuscationChain(obfuscatedScript, deobfsSteps) {
+    let deobfs = obfuscatedScript
+    for(const func of deobfsSteps) {
+        deobfs = await deobfuscationLoop(deobfs, func)
+    }
+    return deobfs
+}
+
+const synchrony = new Deobfuscator()
+const webcrackStep = async (x) => await webcrack(x).code
+const synchronyStep = async (x) => await synchrony.deobfuscateSource(x)
+const checkDeobfs = (x) => x.indexOf("<video />") !== -1
+
+async function getDeobfuscatedScript() {
+    const obfuscatedScript = await fetch("https://vidplay.lol/assets/mcloud/min/embed.js?v=655f0689", {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/120.0",
+            "Referer": "https://vidplay.lol/",
+            "Origin": "https://vidplay.lol"
         }
-    }
+    }).then(async (x) => await x.text())
 
-    return synchronyDeobfs
+    const firstTry = await deobfuscationChain(obfuscatedScript, [webcrackStep, synchronyStep])
+    if (checkDeobfs(firstTry)) return firstTry
+
+    const secondTry = await deobfuscationChain(obfuscatedScript, [synchronyStep])
+    return secondTry
 }
 
-// Lazyness moment
-async function getValidScript() {
-    for (let attempts = 0; attempts < 5; attempts++) {
-        const obfuscatedScript = await fetch("https://vidplay.lol/assets/mcloud/min/embed.js", {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/118.0"
-            }
-        }).then(async (x) => await x.text())
+const deobfuscated = await getDeobfuscatedScript() 
 
-        const deobfuscated = await deobfuscate(obfuscatedScript)
-        if (deobfuscated.includes("<video />")) return deobfuscated
-    }
-    throw Error("My condolences")
+// Phase 4: Let's find the keys!
+if (checkDeobfs(deobfuscated)) {
+    const start = deobfuscated.substring(deobfuscated.indexOf("<video />"))
+    const end = start.substring(0, start.indexOf(".replace"))
+    const keys = Array.from(end.matchAll(/'(\w+)'/g), x => x[1])
+    assert(keys.length == 2, "Invalid array length!")
+
+    // Be happy!
+    console.info("Success!")
+    await writeFile("keys.json", JSON.stringify(keys), "utf8")
+} else {
+    // ... Or not xD
+    console.error("FAIL!")
+    await writeFile("failed.js", deobfuscated, "utf8")
 }
-
-const deobfuscated = await getValidScript() 
-// Phase 3: Let's find the plaintext keys!
-const start = deobfuscated.substring(deobfuscated.indexOf("<video />"))
-const end = start.substring(0, start.indexOf(".replace"))
-const keys = Array.from(end.matchAll(/'(\w+)'/g), x => x[1])
-assert(keys.length == 2, "Invalid array length!")
-
-// Be happy!
-await writeFile("keys.json", JSON.stringify(keys), "utf8")
